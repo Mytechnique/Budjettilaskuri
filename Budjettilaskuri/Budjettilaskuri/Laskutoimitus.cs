226826264f7ca5f723434c13d1d04f1f380763d8
Shaedris <joona.kuusisto@hotmail.com>
2017-05-20 04:12:16 +0300

    Add files via upload

---
+++ Budjettilaskuri/Budjettilaskuri/Laskutoimitus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budjettilaskuri
+{
+    class Laskutoimitus
+    {
+        public int _nettotulo; //brutto - vero
+        public int _palkanerotus; //nettotulo - menot
+        public int _hsaastot;//palkanerotus - haluttu säästö
+
+        public Laskutoimitus(int _nettotulo, int _palkanerotus, int _hsaastot)  //Laajalti ottanut saatavilla olevasta materiaalista mallia.
+            //Mahd, että tehnyt väärin/parempia ratkaisuja. Luokkaan laskutoimitusten määritys jotta ne voisi lisätä main metodiin?
+        {
+            
+        }
+
+        public int getNetto()
+        {
+
+            return _nettotulo;
+        }
+        public int getErotus()
+        {
+            return _palkanerotus;
+        }
+        public int getSaastot()
+        {
+            return _hsaastot;
+        }
+    }
+}
---
+++ Budjettilaskuri/Budjettilaskuri/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budjettilaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Budjetti();
+            ReadLine();
+        }
+
+        private static void Budjetti()      //Pitäisi olla toimivasti vikasietoinen, pyytää käyttäjältä tiedon uudestaan ellei syöte ole oikein.
+                                            //Valitettavasti vähäinen tietoni rajauttaa koodaukseni konsoliohjelmiin.
+        {
+            int veroprosentti;
+            int tulot;
+            int menot;
+            int saastot;
+
+            WriteLine("Syötä veroprosentti: ");
+            if (!int.TryParse(ReadLine(), out veroprosentti))
+            {
+                do
+                {
+                    WriteLine("Syöte ei ole kelvollinen. Syötä veroprosentti: ");
+                }
+                while (!int.TryParse(ReadLine(), out veroprosentti));
+            }
+            else
+            {
+                WriteLine($"Veroprosentti on: {veroprosentti}%");
+            }
+
+            WriteLine("Syötä bruttotulo: ");
+            if (!int.TryParse(ReadLine(), out tulot))
+            {
+                do
+                {
+                    WriteLine("Syöte ei ole kelvollinen. Syötä bruttotulo: ");
+                }
+                while (!int.TryParse(ReadLine(), out tulot));
+            }
+            else
+            {
+                WriteLine($"Bruttotulosi on: {tulot}");
+            }
+
+
+            WriteLine("Syötä kulut/kk: ");
+            if (!int.TryParse(ReadLine(), out menot))
+            {
+                do
+                {
+                    WriteLine("Syöte ei ole kelvollinen. Syötä kulut/kk: ");
+                }
+                while (!int.TryParse(ReadLine(), out menot));
+            }
+            else
+            {
+                WriteLine($"Kuukausittaiset kulusi ovat: {menot}");
+            }
+
+            WriteLine("Syötä tavoitesäästö: ");
+            if (!int.TryParse(ReadLine(), out saastot))
+            {
+                do
+                {
+                    WriteLine("Syöte ei ole kelvollinen. Syötä tavoitesäästö: ");
+                }
+                while (!int.TryParse(ReadLine(), out saastot));
+            }
+            else
+            {
+                WriteLine($"Tavoite on: {saastot}");
+            }
+
+        }
+    }
+}
