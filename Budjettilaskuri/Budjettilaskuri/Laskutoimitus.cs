d77a9b0ac2e75c20d7b9727f1be7d94230170d88
Shaedris <joona.kuusisto@hotmail.com>
2017-05-25 02:08:13 +0300

    Add files via upload

--- Budjettilaskuri/Budjettilaskuri/Laskutoimitus.cs
+++ Budjettilaskuri/Budjettilaskuri/Laskutoimitus.cs
@@ -1,35 +1,63 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
 
 namespace BudgetCalculator
 {
     class Calculations
     {
-        public int _netincome; // gross income - tax
-        public int _incomedifference; //net income  - expenses
-        public int _plannedsavings;//income difference - planned savings
+        private double _netincome; // gross income - tax
+        private double _incomedifference; //net income  - expenses
+        private double _plannedsavings; //income difference - planned savings
+        private double _yearlysavings;//savings x 12
+        private double _yearlytax;//income * (taxpercent & 100) * 12
 
-        public Calculations(int _netincome, int _incomedifference, int _plannedsavings)  //Laajalti ottanut saatavilla olevasta materiaalista mallia.
-            //Mahd, että tehnyt väärin/parempia ratkaisuja. Luokkaan laskutoimitusten määritys jotta ne voisi lisätä main metodiin?
+        public Calculations(double netincome, double incomedifference, double plannedsavings, double yearlytax, double yearlysavings)
         {
-            
-        }
+            _netincome = netincome;
+            _incomedifference = incomedifference;
+            _plannedsavings = plannedsavings;
+            _yearlysavings = yearlysavings;
+            _yearlytax = yearlytax;
 
-        public int getnetIncome()
+            double taxpercent;
+            double income;
+            double expenses;
+            double savings;
+
+            taxpercent = double.Parse(ReadLine());
+            income = double.Parse(ReadLine());
+            expenses = double.Parse(ReadLine());
+            savings = double.Parse(ReadLine());
+
+            yearlytax = income * (taxpercent % 100) * 12;
+            incomedifference = netincome - expenses;
+            plannedsavings = incomedifference - savings;
+            yearlysavings = plannedsavings * 12;
+        }
+        public void getNetincome(double netincome)
         {
-
-            return _netincome;
+            _netincome = netincome;
         }
-        public int getDifference()
+        public double getIncomedifference(double incomedifference)
         {
             return _incomedifference;
         }
-        public int getBudget()
+        public double getPlannedsavings(double plannedsavings)
         {
             return _plannedsavings;
+        }
+        public double getYearlytax(double yearlytax)
+        {
+            return _yearlytax;
+        }
+        public double getYearlysavings(double yearlysavings)
+        {
+            return _yearlysavings;
         }
     }
 }
--- Budjettilaskuri/Budjettilaskuri/Program.cs
+++ Budjettilaskuri/Budjettilaskuri/Program.cs
@@ -15,58 +15,62 @@ namespace BudgetCalculator
             ReadLine();
         }
 
-        private static void Budget()      //Should be fault tolerant, asks user input again if incorrect.
-        {
-            int taxpercent;
-            int income;
-            int expenses;
-            int savings;
+        private static void Budget()
+       { 
+            double taxpercent;
+            double income;
+            double expenses;
+            double savings;
 
             WriteLine("Please insert your tax percent: ");
-            if (!int.TryParse(ReadLine(), out taxpercent))
+            if (!double.TryParse(ReadLine(), out taxpercent))
             {
                 do
                 {
                     WriteLine("Input invalid. Please input your tax percent: ");
                 }
-                while (!int.TryParse(ReadLine(), out taxpercent));
+                while (!double.TryParse(ReadLine(), out taxpercent));
             }
             WriteLine($"Your tax percent is: {taxpercent}%");
 
 
             WriteLine("Please insert gross income: ");
-            if (!int.TryParse(ReadLine(), out income))
+            if (!double.TryParse(ReadLine(), out income))
             {
                 do
                 {
                     WriteLine("Input invalid. Please input your gross income: ");
                 }
-                while (!int.TryParse(ReadLine(), out income));
+                while (!double.TryParse(ReadLine(), out income));
             }
             WriteLine($"Your gross income is: {income}");
 
 
             WriteLine("Please insert your monthly expenses: ");
-            if (!int.TryParse(ReadLine(), out expenses))
+            if (!double.TryParse(ReadLine(), out expenses))
             {
                 do
                 {
                     WriteLine("Input invalid. Please input your expenses: ");
                 }
-                while (!int.TryParse(ReadLine(), out expenses));
+                while (!double.TryParse(ReadLine(), out expenses));
             }
             WriteLine($"Your monthly expenses are: {expenses}");
 
             WriteLine("Please insert planned savings: ");
-            if (!int.TryParse(ReadLine(), out savings))
+            if (!double.TryParse(ReadLine(), out savings))
             {
                 do
                 {
                     WriteLine("Input invalid. Please input your planned savings: ");
                 }
-                while (!int.TryParse(ReadLine(), out savings));
+                while (!double.TryParse(ReadLine(), out savings));
             }
             WriteLine($"Planned savings: {savings}");
+
+
+
+            WriteLine($"Net income: \nIncome after expenses:\n Monthly savings:\n Yearly savings:\n Monthly tax:\n Yearly tax:");
         }
     }
 }
