0207098f8dfd5dd8b6fe1e432be440327e5abb90
Shaedris <joona.kuusisto@hotmail.com>
2017-06-04 01:49:40 +0300

    Add files via upload

---
+++ Budjettilaskuri/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Budget();
+            ReadLine();
+        }
+
+        private static void Budget()
+        {
+            double taxpercent;
+            double income;
+            double expenses;
+            double savings;
+            
+
+            WriteLine("Please insert your tax percent: ");
+            if (!double.TryParse(ReadLine(), out taxpercent))
+            {
+                do
+                {
+                    WriteLine("Input invalid. Please input your tax percent: ");
+                }
+                while (!double.TryParse(ReadLine(), out taxpercent));
+            }
+            WriteLine($"Your tax percent is: {taxpercent}%");
+
+
+            WriteLine("Please insert gross income: ");
+            if (!double.TryParse(ReadLine(), out income))
+            {
+                do
+                {
+                    WriteLine("Input invalid. Please input your gross income: ");
+                }
+                while (!double.TryParse(ReadLine(), out income));
+            }
+            WriteLine($"Your gross income is: {income}");
+
+
+            WriteLine("Please insert your monthly expenses: ");
+            if (!double.TryParse(ReadLine(), out expenses))
+            {
+                do
+                {
+                    WriteLine("Input invalid. Please input your expenses: ");
+                }
+                while (!double.TryParse(ReadLine(), out expenses));
+            }
+            WriteLine($"Your monthly expenses are: {expenses}");
+
+            WriteLine("Please insert planned savings: ");
+            if (!double.TryParse(ReadLine(), out savings))
+            {
+                do
+                {
+                    WriteLine("Input invalid. Please input your planned savings: ");
+                }
+                while (!double.TryParse(ReadLine(), out savings));
+            }
+            WriteLine($"Planned savings: {savings}");
+
+            double netincome =  income - (((float)taxpercent / 100.0));
+            double incomedifference = netincome - expenses;
+
+
+
+            WriteLine($"Net income:{Math.Round(netincome,2)} \nIncome after expenses:{Math.Round(incomedifference, 2)}\n Monthly savings:{savings}\n Yearly savings:{savings * 12}\n Monthly tax:{income * (taxpercent / 100.0)}\n Yearly tax:{income * (taxpercent / 100.0) * 12}");
+        }
+    }
+}
--- Budjettilaskuri/Properties/AssemblyInfo.cs
+++ Budjettilaskuri/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d42f7280-85ac-4118-a5ca-8f69ae8530ac")]
+[assembly: Guid("7466e312-a7db-4e4f-befc-df1adafa6f9c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
